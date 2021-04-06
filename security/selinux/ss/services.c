2507dd5a674d3d29a0cc4bc8970498b2b3eea248
Albert I <kras@raphielgang.org>
2021-04-06 16:57:49 +0800

    selinux: Restore auditing support
    This is actually a requirement for VTS. If anyone insists on disabling this,
    use audit=0 cmdline instead to disable audit framework on boot.
    
    * Revert "defconfig: Disable audit"
      This reverts commit d91412f276a2114a4b0808df4080134915a73ac0.
    
    * Revert "selinux: don't require auditing"
      This reverts commit 6fcc86bfc35471488031886167156e380b128aa4.
    
     Conflicts:
    	arch/arm64/configs/X00T_defconfig
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3530,7 +3530,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3553,7 +3552,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
